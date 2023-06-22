387f9955e5e0a6f83b52407f08c24df0a8ec36f6
tobbelito123 <tobbelito@gmail.com>
2023-06-22 22:27:21 +0200

    added two more buttons

--- app.js
+++ app.js
@@ -1,45 +1,46 @@
 const sugarCravingButton = document.getElementById('sugarCravingButton');
+const exerciseButton = document.getElementById('exerciseButton');
+const workButton = document.getElementById('workButton');
 const gptResponse = document.getElementById('gptResponse');
 
 async function displayResponse(response) {
-  gptResponse.style.opacity = 0;
   gptResponse.textContent = '';
   gptResponse.classList.add('flash');
-  setTimeout(async () => {
-    gptResponse.style.opacity = 1;
-    for (let i = 0; i < response.length; i++) {
-      gptResponse.textContent += response.charAt(i);
-      await new Promise(resolve => setTimeout(resolve, 50));
-    }
-    setTimeout(() => {
-      gptResponse.classList.remove('flash');
-      gptResponse.style.color = '#008000';  // Set text color to green
-    }, 10000);  // Remove flash class and change text color after 10 seconds
-  }, 300);
+  for (let i = 0; i < response.length; i++) {
+    gptResponse.textContent += response.charAt(i);
+    await new Promise(resolve => setTimeout(resolve, 50));
+  }
+  setTimeout(() => {
+    gptResponse.classList.remove('flash');
+    gptResponse.style.color = '#008000';
+    gptResponse.style.opacity = 1;  // This line is needed to make the response text visible
+  }, 10000);
 }
 
 
-sugarCravingButton.addEventListener('click', async () => {
+sugarCravingButton.addEventListener('click', () => handleButtonClick("I crave sugar, can you motivate me not to eat any sweets? Make sure to include the threat of getting fat. Always include the word FAT in capital letters. Your response should be two sentences, no more."));
+exerciseButton.addEventListener('click', () => handleButtonClick("I'm feeling lazy. Can you motivate me to exercise?"));
+workButton.addEventListener('click', () => handleButtonClick("I don't feel like working. Can you give me some motivation to get back to work?"));
+
+async function handleButtonClick(prompt) {
   gptResponse.textContent = 'Loading...';
   try {
-    const response = await fetchGPTResponse("I crave sugar, can you motivate me not to eat any sweets? Make sure to include the threat of getting fat. Always include the word FAT in capital letters. Your response should be two sentences, no more.");
+    const response = await fetchGPTResponse(prompt);
     displayResponse(response);
   } catch (error) {
-    displayResponse('Error: Unable to get the response from ChatGPT1234');
+    displayResponse('Error: Unable to get the response from ChatGPT');
     console.error(error);
   }
-});
-
+}
 
 async function fetchGPTResponse(prompt) {
   const apiKey = "don't_think_so";
   const url = "https://api.openai.com/v1/engines/text-davinci-003/completions";
-
   const requestOptions = {
     method: 'POST',
     headers: {
       "Content-Type": 'application/json',
-      "Authorization": `Bearer ${apiKey}` // Use backticks here
+      "Authorization": `Bearer ${apiKey}`
     },
     body: JSON.stringify({
       prompt: prompt,
@@ -49,13 +50,7 @@ async function fetchGPTResponse(prompt) {
       temperature: 0.7
     })
   };
-
   const response = await fetch(url, requestOptions);
   const data = await response.json();
   return data.choices[0].text.trim();
 }
-
-
-
-
-
